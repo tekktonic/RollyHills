f2bb9e3e48d9a2c2e5afad03957caf75ab438968
Daniel Wilkins Daniel.Wilkins@kpit.com <Daniel.Wilkins@kpit.com>
2018-03-08 15:10:21 -0500

    Add a test to ensure that the map stays valid.

--- test_map.py
+++ test_map.py
@@ -5,3 +5,14 @@ def test_map_instantiation():
     """Make sure that map instantiation creates a map of a valid size"""
     map_var = game_map.Map()
     assert len(map_var.map) == variables.MAP_WIDTH
+
+
+def test_map_validity():
+    """Ensure that map remains valid even when generating new terrain"""
+    map_var = game_map.Map()
+
+    for i in range(0, variables.MAP_WIDTH):
+        map_var.step()
+    for i in range(0, variables.MAP_WIDTH):
+        assert map_var.map[i] >= 0 and map_var.map[i] <= 10
+    
