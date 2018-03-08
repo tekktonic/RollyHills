740c62d4bfca8c6408ad11c63aeea4e105ce3a8d
William Spicer <william@here.com>
2018-03-08 12:22:20 -0500

    Classes

---
+++ globals.py
@@ -0,0 +1,8 @@
+"""Globals const used by all classes"""
+jump_height = 3
+map_height = 15
+map_width= 30
+
+
+
+
---
+++ map.py
@@ -0,0 +1,18 @@
+import random
+import globals
+
+class Map:
+    """Our map """
+    def generate(self):
+        self.map[0]
+        for i in range(0,len())
+    def __init__(self):
+        self.map = [0]
+
+
+
+    def slice(self,prev):
+        """Move the map to the left"""
+        self.map[prev+1]= self.map[prev] + (random.randrange(0,globals.jump_height) * random(-1,1))
+
+
