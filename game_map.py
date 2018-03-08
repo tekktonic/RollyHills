806f027c6055f63fa477e6d9534bb3b94dbc0c7b
Daniel Wilkins Daniel.Wilkins@kpit.com <Daniel.Wilkins@kpit.com>
2018-03-08 14:33:14 -0500

    Enable drawing the map from the main game loop.

--- game_map.py
+++ game_map.py
@@ -8,7 +8,9 @@ class Map:
         """Generate our initial map"""
         self.map.insert(0, random.randrange(0, 9))
         for i in range(1, variables.MAP_WIDTH):
-            self.slice(i - 1)
+            self.map.append(max(0, self.map[i-1]
+                                + (random.randrange(0, variables.JUMP_HEIGHT)
+                                   * random.randrange(-1, 2))))
 
     def __init__(self):
         self.map = []
@@ -17,9 +19,9 @@ class Map:
 
     def slice(self, prev):
         """Move the map to the left"""
-        self.map.insert(prev + 1, (self.map[prev]
-                                   + (random.randrange(0, variables.JUMP_HEIGHT)
-                                      * random.randrange(-1, 1))))
+        self.map[prev + 1] = max(0, self.map[prev]
+                                   + min(0, (random.randrange(0, variables.JUMP_HEIGHT)
+                                      * random.randrange(-1, 2))))
 
     def step(self):
         """"move all the array contents left by 1"""
