09d2c7badc434334286e8031f37f8050074da5f5
Daniel Wilkins Daniel.Wilkins@kpit.com <Daniel.Wilkins@kpit.com>
2018-03-08 12:31:30 -0500

    Add display, fix requirements.txt

---
+++ display.py
@@ -0,0 +1,20 @@
+"""Abstract away the system and allow us to work nicely with the display as if it were a good canvas"""
+from sys import stdout
+
+def display(score, game_map, player):
+    """Display the gamestate in the user's terminal"""
+    for line in range(0,15):
+        if line == 0:
+            score_text = "score: " + str(score)
+            for character in range(0, 30 - len(score_text) + 1):
+                stdout.write(' ')
+            stdout.write(score_text)
+        else:
+            for character in range(0, 30):
+                if (player.y == (15 - line) and player.x == character):
+                    stdout.write('@')
+                elif game_map[character] > (15 - line):
+                    stdout.write('#')
+                else:
+                    stdout.write(' ')
+        stdout.write("\n")
