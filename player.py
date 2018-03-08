90bcdca4676e196ccb2003de21b6e838b0c425d0
Anon <william@here.com>
2018-03-08 15:58:41 -0500

    yup

--- player.py
+++ player.py
@@ -11,18 +11,18 @@ class Player():
 
     def update(self, game_map):
         self.y += self.dy
-        self.grav(game_map)
+        #self.grav(game_map)
         if self.y < game_map.map[self.x]:
             self.x -= 1
         elif(self.x <0):
             exit(0)
 
 
- #   def grav(self, game_map):
-    #    if(self.y) - 1 >= game_map.map[self.x]:
-     #       self.dy = max((self.y - game_map.map[self.x])* -1 , self.dy-1)
-       # else:
-         #   self.dy = 0
+   def grav(self, game_map):
+      if(self.y) - 1 >= game_map.map[self.x]:
+          self.dy = max((self.y - game_map.map[self.x])* -1 , self.dy-1)
+       else:
+          self.dy = 0
 
     def jump(self):
          self.dy = variables.JUMP_HEIGHT
