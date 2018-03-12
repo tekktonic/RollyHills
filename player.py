8e92fd4cb65f4482eb86633934c0b9e0c95f869e
Daniel Wilkins Daniel.Wilkins@kpit.com <Daniel.Wilkins@kpit.com>
2018-03-12 10:37:33 -0400

    Allow player to climb.

--- player.py
+++ player.py
@@ -14,17 +14,20 @@ class Player():
         self.grav(game_map)
         if self.y < game_map.map[self.x]:
             self.x -= 1
+        elif self.y == game_map.map[self.x]:
+            self.y += 1
+        
         if (game_map.map[self.x] == self.y - 1):
             self.onclick = False
-        if(self.x <0):
+        if(self.x < 0):
             exit(0)
 
 
 
 
     def grav(self, game_map):
-        if(self.y) - 1 >= game_map.map[self.x]:
-            self.dy = max((self.y - game_map.map[self.x])* -1 , self.dy-1)
+        if (self.y - 1) >= game_map.map[self.x]:
+            self.dy = max((self.y - game_map.map[self.x]) * -1 , self.dy - 1)
         else:
             self.dy = 0
 
